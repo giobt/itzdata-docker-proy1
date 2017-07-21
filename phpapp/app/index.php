6217f5955276a757eeb7e3963bf9216f081d1bd6
Giorgio Balconi <giobt@hotmail.com>
2017-07-20 19:48:12 -0600

    docker compose project complete

---
+++ phpapp/app/index.php
@@ -0,0 +1,14 @@
+<?php
+$mysql_host = "localhost";
+$mysql_user = "username";
+$mysql_pass = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
